42350dab639f5cc44d9725130ce25da2c22630d1
phu nov <phuephue1125@gmail.com>
2024-01-15 23:26:51 +0630

    two-promise-func

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,68 @@
-import * as express from 'express';
-import e = require('express');
+import * as express from "express";
 
 // namespace declare -->express.Application
 let app: express.Application = express();
 
-
-
-function Math_ByX(){
-    return false;
+function Math_ByX() {
+  return true;
 }
-function Math_ByY(){
-    return true;
+function Math_ByY() {
+  return false;
 }
 
-function result():Promise<string>{
-return new Promise ((resolve,reject)=>{
-if(Math_ByX()){
-    resolve("math By X arrive");   
-}else if(Math_ByY()){
-    resolve("this is Math By Y arrive");
-}else{
-    reject("Nothing at both arrive");
+function result(): Promise<string> {
+  return new Promise((resolve, reject) => {
+    if (Math_ByX()) {
+      resolve("math By X arrive");
+    } else if (Math_ByY()) {
+      resolve("this is Math By Y arrive");
+    } else {
+      reject("Nothing at both arrive");
+    }
+  });
 }
-
-})
+//
+function Java() {
+  return false;
 }
+function NodeJS() {
+  return false;
+}
+function resultLanguageBook() {
+  return new Promise((resolve, reject) => {
+    if (Java()) {
+      resolve("java" + true);
+    } else if (NodeJS()) {
+      resolve("nodejs " + true);
+    } else {
+      reject("reject " + false);
+    }
+  });
+}
+async function resultBook() {
+  try {
+    let book = await resultLanguageBook();
+    return book;
+  } catch (e) {
+    return e;
+  }
+}
+resultBook()
+  .then((result) => {
+    if (result) {
+      resultLanguageBook()
+        .then((data) => {
+          console.log("Language: " + data);
+        })
+        .catch((error) => {
+          // reject true in error
+          console.log("error: ", error);
+        });
+    }
+  })
+  .catch((e) => {
+    return e;
+  });
 // result().then((result)=>{
 // console.log(result)
 // }).catch(error=>{
@@ -33,25 +71,27 @@ if(Math_ByX()){
 
 // async await function
 
-async function final(){
-// let res=await result();
-// return res;
-try{
- let res=await result();
- return res;
-}catch(e){
-return Promise.reject(e);
+async function final() {
+  try {
+    let res = await result();
+    // if math result true , auto include LanguageBook result.
+    if (res) res = await resultBook();
+    // will be return the same
+    return res;
+  } catch (e) {
+    return e;
+  }
 }
-}
-final().then((res)=>{
-console.log(res);
-}).catch(e=>{
-    console.log(e)
-})
-
+final()
+  .then((data) => {
+    console.log("Math : " + data);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
 // asynchronous
-let port=3000;
-app.listen(port,()=>{
-    console.log(`server is running at port ${port}`);
-})
+let port = 3000;
+app.listen(port, () => {
+  console.log(`server is running at port ${port}`);
+});
