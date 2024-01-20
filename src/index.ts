55c6d95821d406c4665f7e7fe2b3d3418f4672e8
phu nov <phuephue1125@gmail.com>
2024-01-20 13:46:26 +0630

    connect

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as express from "express";
-
+import * as mongoose from "mongoose";
 let app: express.Application = express();
 
 // as middleware
@@ -8,68 +8,75 @@ app.use((req, res, next) => {
   next();
 });
 
+mongoose
+  .connect(
+    "mongodb+srv://willaustin:willaustin@testnodejs.cvcyxuo.mongodb.net/testnodejs?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("mongodb connected");
+  });
+
 app.get("/api/user/test", (req, res) => {
-  res.send("user test");
+  res.status(200).send("user test");
 });
 
-app.get(
-  "/api/user/login",
-  (req, res, next) => {
-    console.log("user login");
-    const data = [{ name: "will_austin" }];
-    res.send(data);
-    next();
-  }
-
-  // ,
+// app.get(
+//   "/api/user/login",
+//   (req, res, next) => {
+//     console.log("user login");
+//     const data = [{ name: "will_austin" }];
+//     res.send(data);
+//     next();
+//   }
+ // ,
   // (req, res) => {
   //   console.log("middeware 2");
   //   res.status(200).send("test");
   // }
-);
+// );
 //
 
-app.use((req, res, next) => {
-  console.log("middleware 1");
-  next();
-});
-const object1 = { id: 1, name: "bae" };
-// ...spread operator convert as single element
-// change value in{}
-const object3 = { age: 24, location: "Nay pyi taw" };
-const object2 = {
-  ...object1,
-  email: "ppw112@gmail.com",
-  password: 111,
-  id: 4,
-  name: "thalthal",
-  ...object3,
-};
-console.log(object2);
+// app.use((req, res, next) => {
+//   console.log("middleware 1");
+//   next();
+// });
+// const object1 = { id: 1, name: "bae" };
+// // ...spread operator convert as single element
+// // change value in{}
+// const object3 = { age: 24, location: "Nay pyi taw" };
+// const object2 = {
+//   ...object1,
+//   email: "ppw112@gmail.com",
+//   password: 111,
+//   id: 4,
+//   name: "thalthal",
+//   ...object3,
+// };
+// console.log(object2);
 
-const array = [1, 2, 3];
-const array2 = [4, 5, 6];
-const nextArray = [...array, ...array2];
-console.log(nextArray);
+// const array = [1, 2, 3];
+// const array2 = [4, 5, 6];
+// const nextArray = [...array, ...array2];
+// console.log(nextArray);
 
-app.get("/api/user/login", (req, res) => {
-  console.log("email : " + req.query.email);
-  res.send("success get method");
-});
+// app.get("/api/user/login", (req, res) => {
+//   console.log("email : " + req.query.email);
+//   res.send("success get method");
+// });
 
-app.post("/api/login", (req, res) => {
-  res.json({
-    name: "ppw",
-    age: 24,
-  });
-  console.log("success");
-  // console.log(req)
-});
-// app.put();
-app.patch("/api/user/password", (req, res) => {
-  res.send("send patch");
-});
-// app.delete();
+// app.post("/api/login", (req, res) => {
+//   res.json({
+//     name: "ppw",
+//     age: 24,
+//   });
+//   console.log("success");
+//   // console.log(req)
+// });
+// // app.put();
+// app.patch("/api/user/password", (req, res) => {
+//   res.send("send patch");
+// });
+// // app.delete();
 
 app.listen(3000, () => {
   console.log("server listening on port 3000");
